73dfaf2aa76ff35d92bc8f6ae625b93aa7e759a3
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-09-18 14:49:04 -0600

    Populate virtual machine table

--- src/mission-control/compute/virtual-machine/virtualmachine-page.tsx
+++ src/mission-control/compute/virtual-machine/virtualmachine-page.tsx
@@ -5,8 +5,11 @@ import { VirtualMachine } from "./virtualmachine"
 
 enum sortBy {
     name = "name",
-    namespace = "namespace",
+    location = "location",
+    machinetype = "machinetype",
+    image = "image",
     age = "age",
+    network = "network",
 }
 
 export class VirtualMachinePage extends React.Component<{ extension: Renderer.LensExtension }> {
@@ -17,6 +20,10 @@ export class VirtualMachinePage extends React.Component<{ extension: Renderer.Le
           className="VirtualMachine" store={virtualMachineStore}
           sortingCallbacks={{
             [sortBy.name]: (virtualmachine: VirtualMachine) => virtualmachine.getName(),
+            [sortBy.location]: (virtualmachine: VirtualMachine) => virtualmachine.spec.forProvider.location,
+            [sortBy.machinetype]: (virtualmachine: VirtualMachine) => virtualmachine.spec.forProvider.machineType,
+            [sortBy.image]: (virtualmachine: VirtualMachine) => virtualmachine.spec.forProvider.image,
+            [sortBy.network]: (virtualmachine: VirtualMachine) => virtualmachine.spec.forProvider.network,
             [sortBy.age]: (virtualmachine: VirtualMachine) => virtualmachine.metadata.creationTimestamp
           }}
           searchFilters={[
@@ -25,12 +32,18 @@ export class VirtualMachinePage extends React.Component<{ extension: Renderer.Le
           renderHeaderTitle="VirtualMachine"
           renderTableHeader={[
             { title: "Name", className: "name", sortBy: sortBy.name },
-            { title: "Machine Type", className: "machinetype"},
+            { title: "Location", className: "location", sortBy: sortBy.location },
+            { title: "Machine Type", className: "machinetype", sortBy: sortBy.machinetype },
+            { title: "Image", className: "image", sortBy: sortBy.image },
+            { title: "Network", className: "network", sortBy: sortBy.network },
             { title: "Age", className: "age", sortBy: sortBy.age },
           ]}
           renderTableContents={(virtualmachine: VirtualMachine) => [
             virtualmachine.getName(),
+            virtualmachine.spec.forProvider.location,
             virtualmachine.spec.forProvider.machineType,
+            virtualmachine.spec.forProvider.image,
+            virtualmachine.spec.forProvider.network,
             virtualmachine.getAge()
           ]}
         />
