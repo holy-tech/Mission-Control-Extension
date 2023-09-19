896c0c24bb4e1f2882c88fa4a05d46630732e06e
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-09-19 11:40:19 -0600

    Add page to renderer

--- renderer.tsx
+++ renderer.tsx
@@ -2,12 +2,15 @@ import { Renderer } from "@k8slens/extensions";
 import { ExampleIcon, MissionPage } from "./src/mission-control/crossplane-config/missions/missions-page"
 import { MissionKeyPage } from "./src/mission-control/crossplane-config/missionkeys/missionkeys-page"
 import { VirtualMachinePage } from "./src/mission-control/compute/virtual-machine/virtualmachine-page"
+import { StorageBucketPage } from "./src/mission-control/storage/storage-bucket/storagebucket-page"
 import { Mission } from "./src/mission-control/crossplane-config/missions/missions"
 import { MissionKey } from "./src/mission-control/crossplane-config/missionkeys/missionkeys"
 import { VirtualMachine } from "./src/mission-control/compute/virtual-machine/virtualmachine"
+import { StorageBucket } from "./src/mission-control/storage/storage-bucket/storagebucket"
 import { MissionDetails } from "./src/mission-control/crossplane-config/missions/missions-details"
 import { MissionKeyDetails } from "./src/mission-control/crossplane-config/missionkeys/missionkeys-details"
 import { VirtualMachineDetails } from "./src/mission-control/compute/virtual-machine/virtualmachine-details"
+import { StorageBucketDetails } from "./src/mission-control/storage/storage-bucket/storagebucket-details"
 import React from "react"
 
 const {
@@ -34,6 +37,12 @@ export default class ExampleExtension extends Renderer.LensExtension {
       id: "virtualmachine",
       components: {
         Page: () => <VirtualMachinePage extension={this}/>,
+      }
+    },
+    {
+      id: "storagebucket",
+      components: {
+        Page: () => <StorageBucketPage extension={this}/>,
       }
     }
   ]
@@ -76,6 +85,21 @@ export default class ExampleExtension extends Renderer.LensExtension {
       components: {
         Icon: () => <Icon material="arrow"/>,
       }
+    },
+    {
+      id: "storage",
+      title: "Storage",
+      components: {
+        Icon: ExampleIcon,
+      }
+    },
+    {
+      parentId: "storage",
+      target: { pageId: "storagebucket" },
+      title: "Storage Bucket",
+      components: {
+        Icon: () => <Icon material="arrow"/>,
+      }
     }
   ]
 
@@ -102,6 +126,14 @@ export default class ExampleExtension extends Renderer.LensExtension {
       priority: 10,
       components: {
         Details: (props: Renderer.Component.KubeObjectDetailsProps<VirtualMachine>) => <VirtualMachineDetails {...props} />
+      }
+    },
+    {
+      kind: StorageBucket.kind,
+      apiVersions: ["storage.mission-control.apis.io/v1alpha1"],
+      priority: 10,
+      components: {
+        Details: (props: Renderer.Component.KubeObjectDetailsProps<StorageBucket>) => <StorageBucketDetails {...props} />
       }
     }
   ]
